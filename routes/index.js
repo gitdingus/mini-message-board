977fc5f035c444e44f1d4819de6d147e19644267
Josh S <aaabccc87@protonmail.com>
2023-05-25 20:12:31 -0400

    Define routes for message board to view messages and post new ones

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const messages = [
+  {
+    text: 'Hello! To create a new message, navigate to the view tab and fill out the form!',
+    user: 'Admin',
+    added: new Date(),
+  },
+  {
+     text: 'Hello Message Board!', 
+     user: 'Me',
+     added: new Date(),
+  },
+];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Mini Message Board',
+    messages: messages,
+  });
 });
 
+router.get('/new', function(req, res, next) {
+  res.render('messageform', {
+    title: 'New Message',
+  });
+});
+
+router.post('/new', function(req, res, next) {
+  const newMessage = {
+    text: req.body.message,
+    user: req.body.name,
+    added: new Date(),
+  }
+
+  messages.push(newMessage);
+
+  res.redirect('/');
+});
 module.exports = router;
